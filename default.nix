5b797de82d5999afdc464fa6080edc61a9ae0ac2
Nikolai Schimke <schimkeNikolai@gmail.com>
2025-01-06 11:34:00 +0000

    Add initial project structure with PDM configuration
    - Created .pdm-python file to specify Python interpreter path.
    - Added default.nix and flake.nix for Nix package management.
    - Introduced pdm.lock and pyproject.toml for PDM package management.
    - Updated .gitignore to exclude .pdm-build directory.
    - Added empty __init__.py files for src/blackbird and tests directories.

---
+++ default.nix
@@ -0,0 +1,31 @@
+# An example package with dependencies defined via pyproject.toml
+{
+  config,
+  lib,
+  dream2nix,
+  ...
+}: {
+  imports = [
+    dream2nix.modules.dream2nix.WIP-python-pdm
+  ];
+
+  mkDerivation = {
+    src = lib.cleanSourceWith {
+      src = lib.cleanSource ./.;
+      filter = name: type:
+        !(builtins.any (x: x) [
+          (lib.hasSuffix ".nix" name)
+          (lib.hasPrefix "." (builtins.baseNameOf name))
+          (lib.hasSuffix "flake.lock" name)
+        ]);
+    };
+  };
+  pdm.lockfile = ./pdm.lock;
+  pdm.pyproject = ./pyproject.toml;
+
+  buildPythonPackage = {
+    pythonImportsCheck = [
+      "my_tool"
+    ];
+  };
+}
